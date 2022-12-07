5032dc9e55f7428ccfb363c64fb3a2a9adfcf16b
megakite <megakite214@hotmail.com>
2022-12-07 17:12:52 +0800

    Formatting

--- kToolsAnimation.jsx
+++ kToolsAnimation.jsx
@@ -1,56 +1,53 @@
 (function kToolsAnimation(thisObj) {
 	/* Build UI */
 	function buildUI(thisObj) {
-
 		var win = (thisObj instanceof Panel) ? thisObj : new Window('palette', "kTools");
-			win.orientation = "column";
-			win.alignChildren = "left";
-			win.btn1 = win.add('button', undefined, "Work!");
-			win.grp1 = win.add('group');
-			win.chk1 = win.add('checkbox', undefined, "Separate");
-			win.chk2 = win.add('checkbox', undefined, "Normalize");
-			win.chk3 = win.add('checkbox', undefined, "Apply");
-			win.txt1 = win.add('edittext', [0,0,40,20], 10);
-			win.lbl1 = win.add('statictext', undefined, 90);
-			win.txt3 = win.add('edittext', [0,0,40,20], 0);
-			win.chk4 = win.add('checkbox', undefined, "Inverse");
-			win.lbl2 = win.add('statictext', undefined, "Version IJW");
+		win.orientation = "column";
+		win.alignChildren = "left";
+		win.btn1 = win.add('button', undefined, "Work!");
+		win.grp1 = win.add('group');
+		win.chk1 = win.add('checkbox', undefined, "Separate");
+		win.chk2 = win.add('checkbox', undefined, "Normalize");
+		win.chk3 = win.add('checkbox', undefined, "Apply");
+		win.txt1 = win.add('edittext', [0,0,40,20], 10);
+		win.lbl1 = win.add('statictext', undefined, 90);
+		win.txt3 = win.add('edittext', [0,0,40,20], 0);
+		win.chk4 = win.add('checkbox', undefined, "Inverse");
+		win.lbl2 = win.add('statictext', undefined, "Version IJW");
 
-			win.chk3.value = true;
+		win.chk3.value = true;
 
-			win.txt1.onChange = function() {
-				win.lbl1.text = 100 - parseFloat(win.txt1.text);
-			}
-			win.chk1.onClick = function() {
-				if (this.value) 
-					win.chk2.enabled = win.chk3.enabled = win.chk4.enabled = false;
-				else
-					win.chk2.enabled = win.chk3.enabled = win.chk4.enabled = true;
-			}
-			win.btn1.onClick = function() {
-				work(
-					parseFloat(win.lbl1.text),
-					parseFloat(win.txt1.text),
-					parseFloat(win.txt3.text),
-					win.chk1.value,
-					win.chk2.value && win.chk2.enabled,
-					win.chk3.value && win.chk3.enabled,
-					win.chk4.value && win.chk4.enabled
-				);
-			}
+		win.txt1.onChange = function() {
+			win.lbl1.text = 100 - parseFloat(win.txt1.text);
+		}
+		win.chk1.onClick = function() {
+			if (this.value) 
+				win.chk2.enabled = win.chk3.enabled = win.chk4.enabled = false;
+			else
+				win.chk2.enabled = win.chk3.enabled = win.chk4.enabled = true;
+		}
+		win.btn1.onClick = function() {
+			work(
+				parseFloat(win.lbl1.text),
+				parseFloat(win.txt1.text),
+				parseFloat(win.txt3.text),
+				win.chk1.value,
+				win.chk2.value && win.chk2.enabled,
+				win.chk3.value && win.chk3.enabled,
+				win.chk4.value && win.chk4.enabled
+			);
+		}
 
 		win.layout.layout(true);
 
-		return win
+		return win;
 	}
+
 	buildUI(thisObj);
-	
 
 	/* General functions */
 	function doSeparation(prop, layer) {
-
 		if (prop.value instanceof Array) {
-
 			var tmpStr = '[';
 			var seprProp = layer.effect.addProperty("Pseudo/kTASepr");
 			seprProp.property("Pseudo/kTASepr-0001").expression = "//" + prop.name + '\r' + "value;";
@@ -66,17 +63,13 @@
 				tmpStr += 'effect("' + seprProp.name + '")("Pseudo/kTASepr-000' + (i+2) + '"), ';
 			}
 			prop.expression = tmpStr.slice(0, -2) + ']';
-			
 		}
-
 	}
 
 	function doNormalization(prop, layer) {
-
 		var selKeyfrms = prop.selKeys;
 		// Check if Properties are multi-dimensional
 		if (prop.value instanceof Array) {
-
 			var rngVal = new Array(selKeyfrms.length);
 			for (var i = 0; i < prop.value.length; i++) {
 				var valMax, valMin;
@@ -90,7 +83,6 @@
 				else 
 					rngVal[i] = Math.max(Math.abs(valMax), Math.abs(valMin));
 			}
-			
 			var tmpStr = '[';
 			var nrmlProp = layer.effect.addProperty("Pseudo/kTANrmlMD");
 			nrmlProp.property("Pseudo/kTANrmlMD-0001").expression = "//" + prop.name + "\rvalue;";
@@ -106,9 +98,7 @@
 						  'effect("' + nrmlProp.name + '")("Pseudo/kTANrmlMD-000' + (2*i+3) + '"), ';
 			}
 			prop.expression = tmpStr.slice(0, -2) + "]";
-
 		} else {
-
 			var rngVal, valMax, valMin;
 			for (var j = 1; j < selKeyfrms.length; j++) {
 				var curKeyfrm = selKeyfrms[j];
@@ -133,13 +123,10 @@
 			tmpStr += 'effect("' + nrmlProp.name + '")("Pseudo/kTANrmlSD-0002")*' +
 					  'effect("' + nrmlProp.name + '")("Pseudo/kTANrmlSD-0003"), ';
 			prop.expression = tmpStr.slice(0, -2) + "]";
-
 		}
-
 	}
 
 	function doApplyCurve(prop, I, O, ovs, inv) {
-
 		var selKeyfrms = prop.selKeys;
 		var speeds = new Array(selKeyfrms.length);
 		for (var j = 1; j < selKeyfrms.length; j++) {
@@ -161,15 +148,12 @@
 			prop.setInterpolationTypeAtKey(curKeyfrm, KeyframeInterpolationType.BEZIER, KeyframeInterpolationType.BEZIER);
 			prop.setTemporalEaseAtKey(curKeyfrm, [easeIn], [easeOut]);
 		}
-
 	}
 
 
 	/* Executive function */
 	function work(typedIn, typedOut, overshoot, separation, normalization, applyCurve, inversed) {
-
 		var functionName = "kTools_Work";
-
 		app.beginUndoGroup(functionName);
 		if (app.project && app.project.activeItem && app.project.activeItem instanceof CompItem) {
 			var selLayers = app.project.activeItem.layers;
@@ -192,7 +176,5 @@
 			}
 		}
 		app.endUndoGroup();
-
 	}
-
 })(this);
